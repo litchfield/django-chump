bbd3e610d4d45d8a3ce0f5031e51e07d90958647
Simon Litchfield <simon@s29.com.au>
2014-07-10 17:31:38 +1000

    Added example form html

--- chump/forms.py
+++ chump/forms.py
@@ -1,10 +1,13 @@
 from mailchimp import utils
 from django import forms
 
-
 class ChumpSubscribeForm(forms.Form):
-    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email address', 'required': True, 'type': 'email'}))
+    name = forms.CharField(required=False, 
+                widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
+    email = forms.EmailField(
+                widget=forms.TextInput(attrs={'placeholder': 'Email address', 
+                                              'required': True, 
+                                              'type': 'email'}))
 
     def __init__(self, list_id, *args, **kwargs):
         self.list_id = list_id
--- chump/templatetags/chump_tags.py
+++ chump/templatetags/chump_tags.py
@@ -12,5 +12,5 @@ def get_chump_form(list_id=None):
 @register.inclusion_tag(['chump/chump_form.html', 'includes/chump_form.html', 'inc/chump_form.html', 'partial/chump_form.html', 'chump_form.html'])
 def chump_form(list_id=None):
     list_id = list_id or settings.CHUMP_LIST_ID  # Throw an error if no value
-    return ChumpSubscribeForm(list_id)
+    return {'chump_form':ChumpSubscribeForm(list_id)}
 
